64c3ceda6c8af7b43adda7dcff27e1d1b6390c31
Noland <nolan.bjorn.schuler@gmail.com>
2025-01-09 16:32:53 -0800

    Readded stow
    readded stow

--- src/main/java/frc/robot/subsystems/superstructure/pivot/Pivot.java
+++ src/main/java/frc/robot/subsystems/superstructure/pivot/Pivot.java
@@ -1,14 +1,12 @@
 package frc.robot.subsystems.superstructure.pivot;
 
 import frc.robot.subsystems.superstructure.GenericSuperstructure;
-
+//for intaking a note - TODO: ask the intake people what to do, L4 - 45 degrees, for L2 and L3 - 35 degrees, L1 - TODO: need to test
 public class Pivot extends GenericSuperstructure<Pivot.PivotTarget> {
   public enum PivotTarget implements GenericSuperstructure.PositionTarget {
-    L4(0),    //TODO: fix these modes
-    L3(0),
-    L2(0),
-    L1(53),
-    SHUTTLE(45);
+    STOW(0), //FIXME: Add zeroing code to make sure the robot ACTUALLY goes to 0
+    SCORE(25); //FIXME: this is just a placeholder value to make sure that the pivot actually moves
+    //FIXME: Change score to the separate L4, L3, L2, L1 scoring  
     private double position;
 
     private PivotTarget(double position) {
