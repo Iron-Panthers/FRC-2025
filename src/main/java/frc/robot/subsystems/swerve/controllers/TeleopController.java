4dd1afee457d1b796287d939d0ec9f3b9179cceb
Jay <jaykang445@gmail.com>
2025-01-13 08:45:31 +0000

    refactor 'gyro zeroing' in TeleopController to accept general yaw supplier

--- src/main/java/frc/robot/subsystems/swerve/controllers/TeleopController.java
+++ src/main/java/frc/robot/subsystems/swerve/controllers/TeleopController.java
@@ -8,14 +8,18 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import frc.robot.RobotState;
+import java.util.function.Supplier;
 
 public class TeleopController {
-
+  private final Supplier<Rotation2d> yawSupplier;
   private double controllerX = 0;
   private double controllerY = 0;
   private double controllerOmega = 0;
-  private Rotation2d offset = new Rotation2d();
+
+  /* teleop control with specified yaw supplier, typically "arbitrary" yaw */
+  public TeleopController(Supplier<Rotation2d> yawSupplier) {
+    this.yawSupplier = yawSupplier;
+  }
 
   /* accept driver input from joysticks */
   public void acceptJoystickInput(double controllerX, double controllerY, double controllerOmega) {
@@ -31,22 +35,11 @@ public class TeleopController {
     double omega = MathUtil.applyDeadband(controllerOmega, 0.001);
     omega = Math.copySign(Math.pow(Math.abs(omega), 1.5), omega);
 
-    // eventaully run off of pose estimation?
     return ChassisSpeeds.fromFieldRelativeSpeeds(
         linearVelocity.getX() * DRIVE_CONFIG.maxLinearVelocity(),
         linearVelocity.getY() * DRIVE_CONFIG.maxLinearVelocity(),
         omega * DRIVE_CONFIG.maxAngularVelocity(),
-        RobotState.getInstance().getOdometryPose().getRotation()); // change to estimatedpose
-
-    /* ChassisSpeeds speeds =
-        new ChassisSpeeds(
-            linearVelocity.getX() * DRIVE_CONFIG.maxLinearVelocity(),
-            linearVelocity.getY() * DRIVE_CONFIG.maxLinearVelocity(),
-            omega * DRIVE_CONFIG.maxAngularVelocity());
-    // eventually run off of pose estimation?
-    speeds.toRobotRelativeSpeeds(yaw);
-
-    return speeds; */
+        yawSupplier.get());
   }
 
   public Translation2d calculateLinearVelocity(double x, double y) {
@@ -54,16 +47,12 @@ public class TeleopController {
     double magnitude = MathUtil.applyDeadband(Math.hypot(x, y), 0.1);
     magnitude = Math.pow(magnitude, 1.5);
 
-    Rotation2d theta = new Rotation2d(x, y).plus(offset);
+    Rotation2d theta = new Rotation2d(x, y);
 
     Translation2d linearVelocity =
         new Pose2d(new Translation2d(), theta)
             .transformBy(new Transform2d(magnitude, 0, new Rotation2d()))
             .getTranslation();
     return linearVelocity;
-  }
-
-  public void setGyroOffset(Rotation2d offset) {
-    this.offset = offset;
   }
 }
