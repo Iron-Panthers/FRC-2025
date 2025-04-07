b642cb51e17b8a767e7bac9c22afda9b2f221e5c
Nora <nizcat5@gmail.com>
2025-04-06 18:18:06 -0700

    Actually a coral's width

--- src/main/java/frc/robot/commands/ApproachReef.java
+++ src/main/java/frc/robot/commands/ApproachReef.java
@@ -14,10 +14,10 @@ public class ApproachReef extends SequentialCommandGroup {
   public enum LevelOffsets {
     // metres
     L4_OFFSET(0.14),
-    L3_OFFSET(0.1),
+    L3_OFFSET(0.12),
     PREP_L4_OFFSET(0.5),
-    L2_OFFSET(0.1),
-    L1_OFFSET(0.1);
+    L2_OFFSET(0.12),
+    L1_OFFSET(0.12);
     public double levelOffset;
 
     private LevelOffsets(double levelOffset) {
