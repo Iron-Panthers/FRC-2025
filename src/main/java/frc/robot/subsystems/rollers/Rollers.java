bd13efc52f2a9fcd624a0dfc99827b10291f05fb
Nora <nizcat5@gmail.com>
2025-04-09 16:02:58 -0700

    Fix compile error

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -57,8 +57,8 @@ public class Rollers extends SubsystemBase {
       }
       case FORCE_INTAKE -> {
         intakeTime += 0.02;
-        intake.setVoltageTarget(Intake.Target.INTAKE);
-        funnel.setVoltageTarget(Funnel.Target.INTAKE);
+        intake.setRollerTarget(Intake.Target.INTAKE);
+        funnel.setRollerTarget(Funnel.Target.INTAKE);
         if (intakeTime > 0.5) {
           this.targetState = RollerState.INTAKE;
           intakeTime = 0;
