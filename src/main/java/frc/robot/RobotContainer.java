71c5b4dbf8c282ebc9f7c89d335aea3a87d11f01
Nora <nizcat5@gmail.com>
2025-04-09 20:15:21 -0700

    Got rid of L4 not going to score problem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -500,8 +500,7 @@ public class RobotContainer {
                 !swerve.isTeleop()
                     && DriverStation.isTeleop()
                     && levelOffsets == LevelOffsets.PREP_L4_OFFSET
-                    && (rollers.readyToRaise()
-                        || superstructure.getTargetState() != SuperstructureState.INTAKE))
+                    && (superstructure.getTargetState() != SuperstructureState.INTAKE))
         .onTrue(superstructure.goToStateCommand(SuperstructureState.SCORE_L4));
     // auto go half to L4 after intaking
     new Trigger(() -> levelOffsets == LevelOffsets.PREP_L4_OFFSET && rollers.readyToRaise())
